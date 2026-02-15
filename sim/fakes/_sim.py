3e5e0d89f5f6935165c28176545c18d68d1cf751
Hugh Evans <hugh.evans.dev@gmail.com>
2026-02-15 13:46:41 +0000

    Dwitch out buttons again

--- sim/fakes/_sim.py
+++ sim/fakes/_sim.py
@@ -8,7 +8,7 @@ import sys
 # GPIO button support
 try:
     import RPi.GPIO as GPIO
-    GPIO_PINS = [10, 27, 22, 23, 24, 25]
+    GPIO_PINS = [10, 9, 11, 8, 7, 5]
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     for pin in GPIO_PINS:
