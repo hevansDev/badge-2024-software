1e750b496707751626d9c1671b2069acbd976a6e
Hugh Evans <hugh.evans.dev@gmail.com>
2026-04-20 16:15:11 +0100

    dedupe

--- sim/fakes/_sim.py
+++ sim/fakes/_sim.py
@@ -522,36 +522,6 @@ class Simulation:
                 r, g, b = _sim.led_state[i + 1]
                 _ws_strip.setPixelColor(i, Color(int(r), int(g), int(b)))
             _ws_strip.show()
-        global _ws_strip, _WS_AVAILABLE, _ws_initialised
-        if not _ws_initialised:
-            _ws_initialised = True
-            try:
-                import importlib.util as _ilu
-                _spec = _ilu.spec_from_file_location(
-                    "adafruit_neopixel",
-                    "/home/hugh/.local/share/virtualenvs/sim-BNHWup2C/lib/python3.10/site-packages/neopixel.py"
-                )
-                _np = _ilu.module_from_spec(_spec)
-                _spec.loader.exec_module(_np)
-                import board as _board
-                _ws_strip = _np.NeoPixel(_board.D18, 12, brightness=1.0, auto_write=False, pixel_order=_np.GRB)
-                _WS_AVAILABLE = True
-                print("✓ WS2812B strip initialized (12 LEDs on GPIO18)")
-            except Exception as _ws_err:
-                _WS_AVAILABLE = False
-                print(f"ℹ WS2812B not available: {_ws_err}")
-
-        for i, s in enumerate(_sim.led_state_buf):
-            if _sim.led_state[i] != s:
-                _sim.led_state[i] = s
-                self._led_surface_dirty = True
-
-        if _WS_AVAILABLE and _ws_strip is not None:
-            for i in range(12):
-                r, g, b = _sim.led_state[i + 1]
-                _ws_strip[i] = (int(r), int(g), int(b))
-            _ws_strip.show()
-
 
 _sim = Simulation()
 
